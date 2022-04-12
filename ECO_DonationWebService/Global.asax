0c0eceab7c2cce7e99fde6837dc9090c531a3e15
Yash Shete <universeyash4@gmail.com>
2022-04-12 17:07:13 +0530

    Replaced the project with new template project with javascript files reduced

---
+++ ECO_DonationWebService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ECO_DonationWebService.Global" Language="C#" %>
---
+++ ECO_DonationWebService/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ECO_DonationWebService.Startup))]
+namespace ECO_DonationWebService
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
