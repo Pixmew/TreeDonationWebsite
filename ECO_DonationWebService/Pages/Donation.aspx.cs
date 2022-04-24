6dd06462bda321add6379fd56052a5bce27d31b9
Yash Shete <universeyash4@gmail.com>
2022-04-24 16:14:56 +0530

    added a donation form skeleton to be modified in future

--- ECO_DonationWebService/Pages/Donation.aspx.cs
+++ ECO_DonationWebService/Pages/Donation.aspx.cs
@@ -11,7 +11,23 @@ namespace ECO_DonationWebService.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void Dolar5_Click(object sender, EventArgs e)
+        {
+            amounttextbox.Text = "5";
+        }
+        protected void Dolar10_Click(object sender, EventArgs e)
+        {
+            amounttextbox.Text = "10";
+        }
+        protected void Dolar15_Click(object sender, EventArgs e)
+        {
+            amounttextbox.Text = "15";
+        }
+        protected void Dolar20_Click(object sender, EventArgs e)
+        {
+            amounttextbox.Text = "20";
         }
     }
 }
